202b46acc0957b36d4425269bdc8f1a9f7bfd0c4
Dan McAuley <danmcauley86@gmail.com>
2021-07-28 13:55:40 +0100

    added food, increase snake size and game over functionality

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const canvas = document.getElementById('gameCanvas');
 // set canvas context to 2d
 const ctx = canvas.getContext('2d');
 
-
 // snake starting co-ordinates
 const snake = [
     {x: 300, y:150},
@@ -17,22 +16,25 @@ const snake = [
 let xSpeed = 10;
 let ySpeed = 0;
 
+let xFood = 100;
+let yFood = 100;
 
 let gameSpeed = 150;
+let gameScore = 0;
+let status = 'alive';
 
-
-function main(ms=gameSpeed) {
-
+function drawGame(ms=gameSpeed) {
+    if(status === 'dead') return;
     if (gameOver()) return;
-
-    setTimeout(function onTick() {
-        clear();
-        draw();
-        move();
-        main();
-        drawFood();
-        console.log(snake[0].x)
-    }, ms)
+    clear();
+    draw();
+    move();
+    drawFood();
+    foodEaten();
+    
+    setTimeout(drawGame, ms);
+    console.log(xFood, yFood);
+    console.log(snake.length)
 }
 
 // clear the canvas
@@ -43,19 +45,51 @@ function clear() {
 // end game if snake hits canvas border
 function gameOver() {
     // IF x <= 0 or >= 600, end the game
-    if (snake[0].x < 0 || snake[0].x >= canvas.width) {return true;}
+    if (snake[0].x < 0 || snake[0].x >= canvas.width- 1
+        || snake[0].y < 0 || snake[0].y >= canvas.height) {
+        
+        ctx.font = '30px sans-serif'
+        ctx.fillStyle = 'red';
+        ctx.textAlign = 'center'
+        ctx.fillText('Game Over', canvas.width / 2, canvas.height / 2);
+        
+        return true;
+    }
     //  IF y <= 0 or >= 300, end the game
-    if (snake[0].y < 0 || snake[0].y >= canvas.height) {return true;}
+    // if () {return true;}
 }
 
+// if first segment of snake collides with food, randomly move food
+function foodEaten() {
+    if(xFood === snake[0].x && yFood === snake[0].y) {
+        xFood = Math.ceil(Math.floor(Math.random() * canvas.width)/10)*10;
+        yFood = Math.ceil(Math.floor(Math.random() * canvas.height)/10)*10;
+        return true
+        //console.log(xFood, yFood);
+        // gameScore += 1;
+    }
+}
 
 // automatically move the snake
 function move() {
     const snakeBody = 
     {x: snake[0].x + xSpeed, y:snake[0].y + ySpeed};
     snake.unshift(snakeBody);
+    if(foodEaten()) {
+        document.getElementById('length').innerHTML = `Snake Length: ${snake.length}`;
+        gameScore += 1;
+        document.getElementById('score').innerHTML = `Score: ${gameScore}`;
+    } else {
     snake.pop();
+    }
 }
+
+// add food randomly to canvas
+function drawFood() {
+    ctx.fillStyle = 'green';
+    ctx.fillRect(xFood, yFood, 10, 10);
+}
+
 
 
 // control snake with arrow keys
@@ -97,23 +131,11 @@ function draw() {
     snake.forEach(drawSnake);
 }
 
-// display snake on canvas initial load
-draw();
 
-// add food randomly to canvas
-function drawFood() {
-    xFood = Math.floor(Math.random() * canvas.width + 1);
-    yFood = Math.floor(Math.random() * canvas.height + 1);
-    ctx.fillStyle = 'green';
-    ctx.fillRect(xFood, yFood, 10, 10);
-}
-
+drawGame();
 
 // TODO:
-// 1. add food randomly to canvas
-// 2. remove food when snake intersects and add more food
-// 3. increase size of snake
-// 4. update score and size
+
 // 5. ability to reset game when game over
 // 6. allow user to set game difficulty - change gameSpeed accordingly
 
