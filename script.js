effcadd76bdcb42c1b6dd0a69b71fd780c6b5cc7
Dan McAuley <danmcauley86@gmail.com>
2021-08-03 22:44:31 +0100

    improve score and speed

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const canvas = document.getElementById('gameCanvas');
 // set canvas context to 2d
 const ctx = canvas.getContext('2d');
 
-// set canvas colour
-ctx.fillStyle = "lightgreen";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
 let game;
 let isActive = 1;
+
+let gameText = document.getElementById('gameText');
+let gameScoreText = document.getElementById('score');
+let snakeLengthText = document.getElementById('length');
 
 // snake starting co-ordinates
 let snake = [
@@ -28,127 +28,94 @@ let xFood = 100;
 let yFood = 100;
 
 let gameScore = 0;
-// let gameSpeed = 2;
+let scoreMultiplier = 1;
 
-let canvasText = 'Game Over';
-let canvasFont ='50px sans-serif'
-
+// run the game. difficulty determines the game speed and increase in score
 function startGame(gameSpeed) {
     game = setInterval(drawGame, 1000/gameSpeed);
-    
+    if (gameSpeed == 3) {
+        scoreMultiplier = 1;
+    } else if (gameSpeed == 6) {
+        scoreMultiplier = 2;
+    } else if (gameSpeed == 10) {
+        scoreMultiplier = 3;
+    } else {
+        scoreMultiplier = 4;
+    }
 }
 
-// loop the game and functions
+// game functions in each loop
 function drawGame() {
-    console.log(isActive);
-    // end game and display game over text once snake collides with itself or wall
+    // when game has ended stop and display game over text
     if (isActive === 0) {
-        gameOverText();
+        gameText.innerHTML = 'Game Over';
         clearInterval(game);
-        console.log('hello');
         return;
-    } else {
+    } else { 
         gameOver();
+        gameText.innerHTML = '';
         clear();
         draw();
         move();
         drawFood();
         foodEaten();
-        
-        // console.log(`snake: x:${snake[0].x}, y:${snake[0].y}`);
-        // console.log(`Food: x:${xFood}, y:${yFood}`);
     }
 }
 
-// clear the canvas
-function clear() {
-    ctx.fillStyle = "lightgreen";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    //ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-// draw snake
-function drawSnake(snakePart) {
-    ctx.fillStyle = 'green';
-    ctx.fillRect(snakePart.x, snakePart.y, 20, 20);
-}
 
 // draw snake on canvas
 function draw() {
-    snake.forEach(drawSnake);
+    snake.forEach(function (snakePart){
+        ctx.fillStyle = 'green';
+        ctx.fillRect(snakePart.x, snakePart.y, 20, 20); 
+    })
 }
 
-// add food randomly to canvas
-function drawFood() {
-    // ctx.fillStyle = 'red';
-    // ctx.fillRect(xFood, yFood, 20, 20);
-    ctx.beginPath();
-    ctx.arc(xFood+10, yFood+10, 10 ,0, 2 * Math.PI);
-    ctx.fillStyle = 'red';
-    ctx.fill();
-}
-
-// game over text on canvas once game has ended
-function gameOverText() {
-    ctx.font = canvasFont;
-    ctx.fillStyle = 'red';
-    ctx.textAlign = 'center'
-    ctx.fillText(canvasText, canvas.width / 2, canvas.height / 2);
-}
-
-// end game if snake hits canvas border
+// end game if snake collides with itself or canvas border
 function gameOver() {
     for(let i = 4; i < snake.length; i++) {
         if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
             isActive = 0;
-            return;
         }
-             
     }
     // IF x < 0 or >= 600 or y <0 or >= 300, end the game
-    if (snake[0].x <= 0|| snake[0].x >= canvas.width - 20
-        || snake[0].y <= 0 || snake[0].y >= canvas.height - 20 ) {
+    if (snake[0].x <= -10|| snake[0].x >= canvas.width - 10
+        || snake[0].y <= -10 || snake[0].y >= canvas.height - 10 ) {
         isActive = 0;
-        return;
     }
 }
 
-// if first segment of snake collides with food, randomly move food
+// if first segment of snake collides with food, regenerate food in random position
 function foodEaten() {
     if(xFood === snake[0].x && yFood === snake[0].y) {
         xFood = (Math.ceil(Math.floor(Math.random() * canvas.width)/20)*20)-20;
         yFood = (Math.ceil(Math.floor(Math.random() * canvas.height)/20)*20)-20;
-        return true
+        return true;
     }
 }
 
-// automatically move the snake
+// move the snake
 function move() {
-    const snakeBody = 
-    {x: snake[0].x + xSpeed, y:snake[0].y + ySpeed};
-    // 
+    // add new segment to the front of the snake based on travelling direction
+    const snakeBody = {x: snake[0].x + xSpeed, y:snake[0].y + ySpeed};
     snake.unshift(snakeBody);
 
     // when food is eaten extend the length by 1 and update score and length
     if(foodEaten()) {
-        document.getElementById('length').innerHTML = `Snake Length: ${snake.length}`;
-        gameScore += 1;
-        document.getElementById('score').innerHTML = `Score: ${gameScore}`;
-        if(gameScore % 5 == 0) {
-            gameSpeed += 2;
-        }
+        snakeLengthText.innerHTML = `${snake.length}`;
+        
+        gameScore += scoreMultiplier;
+        gameScoreText.innerHTML = `${gameScore}`;
     } else {
-    snake.pop();
+    snake.pop(); // remove last segment 
     }
 }
 
 
-
 // control snake with arrow keys
 document.addEventListener('keydown', function(event) {
-    //console.log(event);
     if(event.key == 'Enter') {
-        isActive = 1;
+        startGame(6); // start game at normal speed
     }
     
     // IF left arrow pressed and snake is not moving right, move snake 10px right
@@ -177,8 +144,23 @@ document.addEventListener('keydown', function(event) {
 })
 
 
-// reset game, snake position and direction
 
+
+// add food randomly to canvas
+function drawFood() {
+    ctx.beginPath();
+    ctx.arc(xFood+10, yFood+10, 10 ,0, 2 * Math.PI);
+    ctx.fillStyle = 'red';
+    ctx.fill();
+}
+
+// clear the canvas
+function clear() {
+    ctx.fillStyle = "lightgreen";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+// reset game; clear canvas and reset default variables
 function reset() {
     clear();
 
@@ -189,17 +171,14 @@ function reset() {
         {x: 240, y:160}
     ]
     
-    // horizontal and vertical speed
     xSpeed = 20;
     ySpeed = 0;
+
+    gameScore = 0;
+    
+    snakeLengthText.innerHTML = '0';
+    gameScoreText.innerHTML = '0';
+    gameText.innerHTML = '';
     
     isActive = 1;
-
 }
-// TODO:
-
-// 5. ability to reset game when game over - fix reset() function
-// 6. increase score increment as the speed increase
-
-
-
